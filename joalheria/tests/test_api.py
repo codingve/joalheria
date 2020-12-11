c685960b4aff5b4903bf8228097b442e9ba45ed1
Veronica <vntoledo30@gmail.com>
2020-12-11 15:39:36 -0300

    feature/add adding news tests

--- joalheria/tests/test_api.py
+++ joalheria/tests/test_api.py
@@ -24,3 +24,19 @@ def test_request_blank_value():
 
 def test_request_string_value():
     assert requests.get(url+"/product?price=a").status_code == 400
+
+
+def test_has_discount():
+    r = requests.get(url+'/product?price=5000')
+    data = r.json()
+
+    assert r.status_code == 200
+    assert data['sale_discount'] == "500,00"
+
+
+def test_dont_has_discount():
+    r = requests.get(url+'/product?price=500')
+    data = r.json()
+
+    assert r.status_code == 200
+    assert data['sale_discount'] == "0,00"
